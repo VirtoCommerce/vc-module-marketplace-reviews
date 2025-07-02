9d6d2ff08eb9e3033bc068ba1661748f3aa65289
Maksim <maxim.zinchuk@gmail.com>
2025-07-02 14:29:05 +0300

    chore: deps up

--- src/VirtoCommerce.MarketplaceReviewsModule.Web/reviews-app/src/shims-vue.d.ts
+++ src/VirtoCommerce.MarketplaceReviewsModule.Web/reviews-app/src/shims-vue.d.ts
@@ -1,13 +1,18 @@
 /* eslint-disable */
-import { CoreBladeAdditionalSettings } from "@vc-shell/framework";
-import { Ref, Plugin } from "vue";
-import { DynamicGridSchema, DynamicDetailsSchema } from "@vc-shell/framework";
-import * as vue from "vue";
-import * as vueRouter from "vue-router";
-import * as veeValidate from "vee-validate";
-import * as vueI18n from "vue-i18n";
-import moment from "moment";
-import type { Component } from "vue";
+import { CoreBladeAdditionalSettings, DynamicGridSchema, DynamicDetailsSchema } from "@vc-shell/framework";
+
+import type { Component, Ref } from "vue";
+import type {
+  ComponentCustomProperties as _ComponentCustomProperties,
+} from 'vue';
+import type { Composer } from "vue-i18n";
+import type { moment } from "moment";
+import type { Vue } from "vue";
+import type { VueRouter } from "vue-router";
+import type { VeeValidate } from "vee-validate";
+import type { VueI18n } from "vue-i18n";
+import type { VcShellFramework } from "@vc-shell/framework";
+
 
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
@@ -16,23 +21,15 @@ declare module "*.vue" {
 }
 
 declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties {
-    $hasAccess: (permissions: string | string[]) => boolean;
+  interface ComponentCustomProperties extends _ComponentCustomProperties {
+    $mergeLocaleMessage: Composer<{}, {}, {}, string, never, string>["mergeLocaleMessage"];
+    $hasAccess: (permissions: string | string[] | undefined) => boolean;
     $isPhone: Ref<boolean>;
     $isTablet: Ref<boolean>;
     $isMobile: Ref<boolean>;
     $isDesktop: Ref<boolean>;
     $isTouch: boolean;
-    $dynamicModules: {
-      [x: string]: {
-        components?: { [key: string]: Component };
-        composables?: { [key: string]: (...args: any[]) => any };
-        default: { install: (app: any, options?: any) => void };
-        schema: { [key: string]: DynamicGridSchema | DynamicDetailsSchema };
-        locales: { [key: string]: { [key: string]: string } };
-        notificationTemplates?: { [key: string]: Component };
-      };
-    };
+    $t: (key: string, ...args: any[]) => string;
   }
 
   interface ComponentOptionsBase extends CoreBladeAdditionalSettings {}
@@ -40,16 +37,6 @@ declare module "@vue/runtime-core" {
 
 declare global {
   interface Window {
-    VcShellDynamicModules: {
-      [x: string]: {
-        components?: { [key: string]: Component };
-        composables?: { [key: string]: (...args: any[]) => any };
-        default: { install: (app: any, options?: any) => void };
-        schema: { [key: string]: DynamicGridSchema | DynamicDetailsSchema };
-        locales: { [key: string]: { [key: string]: string } };
-        notificationTemplates?: { [key: string]: Component };
-      };
-    };
     Vue: typeof vue;
     VueRouter: typeof vueRouter;
     VeeValidate: typeof veeValidate;
